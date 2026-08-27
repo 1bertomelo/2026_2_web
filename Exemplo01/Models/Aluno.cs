fd3faa88702261721555b3eea51447938a98bc6b
betomelo_9 <betomelo_9@hotmail.com>
2026-08-27 19:46:54 -0300

    Add project files.

---
+++ Exemplo01/Models/Aluno.cs
@@ -0,0 +1,10 @@
+﻿namespace Exemplo01.Models
+{
+    //classe anemica
+    public class Aluno
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string RA { get; set; }
+    }
+}
